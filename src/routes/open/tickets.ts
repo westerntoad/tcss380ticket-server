6c9c9646652000fa9ad1072df337b606ac1f99ef
westerntoad <abr.engebretson@gmail.com>
2024-10-11 00:00:23 -0700

    more robust initial group generation

--- src/routes/open/tickets.ts
+++ src/routes/open/tickets.ts
@@ -13,10 +13,47 @@ interface Group {
     needsHelp: boolean;
     ticketSubmitted: Date;
 }
-let groups: Group[] = [
+function genGroups(): Group[] {
+    const NUM_NAMES: number = 72;
+    const NUM_GROUPS: number = NUM_NAMES / 2;
+    const NAMES: string[] = [
+        "Nigel Bates", "Isabella Huynh", "Reginald Hoover", "Tobias Meyers", "Dorian Berry", "Bessie Villa", "Shelia Gutierrez", "Claudio Shields",
+        "Tommy Russo", "Sam Knox", "Terence Cardenas", "Pete Oconnell", "Grant Mayer", "Nikki Mcbride", "Nona Simon", "Long Perkins",
+        "Sue Wheeler", "Mikel Jacobs", "Romeo Larson", "Leslie Hardin", "Brenda Stevenson", "Letitia Reese", "Bennie Baker", "Alexandra Vaughan",
+        "Minerva Mathis", "Giuseppe Jensen", "Kerry Carroll", "Lavonne Brock", "Hollie Fuller", "Tamika Carlson", "Marion Richmond", "Oswaldo Cox",
+        "Denver Mckee", "Jamaal Edwards", "Lessie Mercer", "Glenna Nielsen", "Renaldo Dickerson", "Serena Li", "Thurman Yates", "Guadalupe Sharp",
+        "Ramona Buchanan", "Gail Weeks", "Dana Roberson", "Lesa Bradford", "Tanisha Flowers", "Roderick Carey", "Angelique Warren", "Rosalind Rosario",
+        "Jamel Tate", "Francesco Hansen", "Isreal Jordan", "Sadie Ritter", "Lazaro Richardson", "Darnell Whitaker", "Ronnie Carney", "Kareem Burton",
+        "Merle Thompson", "Walton Mejia", "Bonita Bullock", "Colton Boone", "Derick Parsons", "Kris Hays", "Rosemary Stuart", "Lillie Cabrera",
+        "Bradly Anderson", "Chang Mullins", "Kristi Deleon", "Louis Bryan", "Darrell Weiss", "Brendon Trevino", "Jodie Strickland", "Madge Hester"
+    ];
+    if (NAMES.length != NUM_NAMES) { throw Error("Invalid number of names.") };
+    
+    let output: Group[] = new Array<Group>(NUM_GROUPS);
+    for(let i = 0; i < NUM_GROUPS; i++) {
+        let needsHelp: boolean = Math.random() < 0.2;
+        let ticketSubmitted: Date = undefined;
+        if (needsHelp) {
+            ticketSubmitted = new Date();
+            ticketSubmitted.setSeconds(Math.floor( Math.random() * 59 ));
+            ticketSubmitted.setMinutes(Math.floor( Math.random() * 59 ));
+        }
+        output[i] = {
+            "groupNum": i + 1,
+            "studentA": NAMES[i * 2],
+            "studentB": NAMES[i * 2 + 1],
+            "needsHelp": needsHelp,
+            "ticketSubmitted": ticketSubmitted
+        };
+    }
+
+    return output;
+}
+let groups: Group[] = genGroups();
+/*let groups: Group[] = [
     {"groupNum": 1, "studentA": "Abe", "studentB": "Jeremiah", "needsHelp": false, "ticketSubmitted": undefined },
     {"groupNum": 2, "studentA": "John", "studentB": "Bob", "needsHelp": true, "ticketSubmitted": new Date() }
-];
+];*/
 //   ~ TODO REPLACE WITH DATABASE ~
 
 
